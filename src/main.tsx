8b5c839974b1a9d8b1ad7ea6948d91265faf43dd
tanakakjs <oneok0813@gmail.com>
2020-06-08 00:31:41 +0900

    Init

---
+++ src/main.tsx
@@ -0,0 +1,4 @@
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+
+ReactDom.render(<div>hello world</div>, document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const webpack = require('webpack');
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = (env, argv) => {
+  const IS_DEVELOPMENT = argv.mode === 'development';
+
+  return {
+    entry: './src/main.tsx',
+    output: {
+      filename: 'app.bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+      rules: [
+        { test: /\.ts(x?)$/, use: [{ loader: 'ts-loader' }, { loader: 'eslint-loader' }] },
+      ],
+    },
+    resolve: {
+      extensions: ['.ts', '.tsx'],
+    },
+    devtool: IS_DEVELOPMENT ? 'source-map' : 'none',
+    devServer: {
+      open: true,
+      contentBase: path.join(__dirname, 'dist'),
+      watchContentBase: true,
+      compress: true,
+      port: 9000,
+    },
+    optimization: {
+      minimizer: IS_DEVELOPMENT
+        ? []
+        : [
+          new TerserPlugin({
+            terserOptions: {
+              compress: { drop_console: true },
+            },
+          }),
+        ],
+    },
+  };
+};
