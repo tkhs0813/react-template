bdde019c2a1cb319995a41e6af5c0a09d7e61333
tanakakjs <oneok0813@gmail.com>
2020-06-19 01:22:14 +0900

    add navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+
+export const Navbar: React.FC = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) =>
+    setIsOpen(!isOpen);
+
+  return (
+    <header className="bg-gray-900 sm:flex sm:justify-between sm:items-center sm:px-2 sm:py-1">
+      <div className="flex items-canter justify-between px-2 py-1 sm:p-0">
+        <div>
+          <img className="h-8" src="/img/logo.svg" alt="logo" />
+        </div>
+        <div className="sm:hidden">
+          <button
+            type="button"
+            onClick={handleClick}
+            className="block text-gray-500 hover:text-white focus:outline-none"
+          >
+            <svg className="h-6 w-6 fill-current" viewBox="0 0 24 24">
+              {isOpen ? (
+                <path
+                  fillRule="evenodd"
+                  d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"
+                />
+              ) : (
+                <path
+                  fillRule="evenodd"
+                  d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
+                />
+              )}
+            </svg>
+          </button>
+        </div>
+      </div>
+      <div
+        className={
+          isOpen
+            ? 'block px-2 pt-2 pb-2 sm:block sm:flex sm: p-0'
+            : 'hidden px-2 pt-2 pb-2 sm:block sm:flex sm: p-0'
+        }
+      >
+        <a
+          href="#"
+          className="block sm:inline-block px-2 py-1 text-white font-semibold hover:bg-gray-800"
+        >
+          List your property
+        </a>
+        <a
+          href="#"
+          className="mt-1 block sm:inline-block px-2 py-1 text-white font-semibold hover:bg-gray-800 sm:mt-0 sm:ml-2"
+        >
+          Trips
+        </a>
+        <a
+          href="#"
+          className="mt-1 block sm:inline-block px-2 py-1 text-white font-semibold  hover:bg-gray-800 sm:mt-0 sm:ml-2"
+        >
+          Messages
+        </a>
+      </div>
+    </header>
+  );
+};
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,14 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
+import { Navbar } from '../components/Navbar';
 
 export const Home: React.FC = () => {
   return (
-    <div>
+    <>
+      <Navbar></Navbar>
       <h1 className="text-5xl font-bold text-purple-500">Hello world!</h1>
       <h1>home</h1>
       <Link to="/about">go to about</Link>
-    </div>
+    </>
   );
 };
